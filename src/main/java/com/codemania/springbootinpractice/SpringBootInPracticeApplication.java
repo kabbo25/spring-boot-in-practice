48eafb64f42d9f86c80e0e360f2470b5c9538f90
kabboghosh <kabbo25@gmail.com>
2025-11-25 10:52:20 +0600

    initial commit

---
+++ src/main/java/com/codemania/springbootinpractice/SpringBootInPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codemania.springbootinpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemania/springbootinpractice/SpringBootInPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemania.springbootinpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
